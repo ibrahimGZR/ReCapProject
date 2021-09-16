4156351a0ecae34bdaa8e2531e88bd3da1eab626
ibrahim.gezer <ibrahim.gezer@IBRAHIMGEZER.etiyacorp.local>
2021-09-16 11:08:29 +0300

    work1

---
+++ src/main/java/com/etiya/ReCapProject/entities/concretes/Color.java
@@ -0,0 +1,27 @@
+package com.etiya.ReCapProject.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "colors")
+public class Color {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "color_id")
+	private int colorId;
+
+	@Column(name = "color_name")
+	private String colorName;
+}
